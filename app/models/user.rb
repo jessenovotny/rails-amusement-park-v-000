c512d3fd1696a06f413585bc62fadcefe48a638c
Jesse Novotny <jessenovotny@gmail.com>
2016-09-09 11:33:01 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  # write associations here
+  has_many :rides
+  has_many :attractions, through: :rides
+  enum role: [:normal, :admin]
+  after_initialize :default_role, if: :new_record?
+  has_secure_password
+
+  def mood
+    nausea > happiness ? "sad" : "happy"
+  end
+
+  def default_role
+    role ||= :normal
+  end
 end
